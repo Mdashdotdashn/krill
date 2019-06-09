b8ae77b1174186fe355c8b6fdf1d250758f4a237
Mdashdotdashn <marc.nostromo@gmail.com>
2019-06-09 15:14:49 +0200

    Allow for sequence not starting with data at the very beginning

--- js/playback/sequence-player.js
+++ js/playback/sequence-player.js
@@ -16,17 +16,32 @@ SequencePlayer.prototype.setSequence = function(sequence)
   this.queued_ = sequence;
 }
 
-SequencePlayer.prototype.advance = function(time)
+SequencePlayer.prototype.cycleLength = function()
+{
+  return math.fraction(this.sequence_ ? this.sequence_.cycleLength_ : "1/1");
+}
+
+SequencePlayer.prototype.cycleTimeAndOffset = function(time)
 {
   var f = math.fraction(time);
-  var cycleLength = math.fraction(this.sequence_ ? this.sequence_.cycleLength_ : "1/1");
+  var cycleLength = this.cycleLength();
   var cycleTime = math.mod(f,cycleLength);
   var offset = math.multiply(math.floor(math.divide(f, cycleLength)), cycleLength);
+  return { cycleTime: cycleTime, offset: offset};
+}
+
+SequencePlayer.prototype.advance = function(time)
+{
 
 //  console.log("-----------------------------"+ time);
 //  console.log("length = "+ cycleLength);
 //  console.log("inner cycle time = "+cycleTime);
 //  console.log("cycle offset = "+offset);
+
+  const cycleTimeAndOffset = this.cycleTimeAndOffset(time);
+  const offset = cycleTimeAndOffset.offset;
+  const cycleTime = cycleTimeAndOffset.cycleTime;
+  const cycleLength = this.cycleLength();
 
   // If we don't have a sequence, we reply we should be triggered
   // at next cycle. We also flag resetCycle so that we queue an incoming
@@ -50,11 +65,12 @@ SequencePlayer.prototype.reset = function()
 {
   // probably a lot more complex than this
   this.resetCycle_ = true;
-  this.current_ = new Step("0", undefined);
+  this.current_ = new Step("0", null);
 }
 
-SequencePlayer.prototype.currentValues = function()
+SequencePlayer.prototype.valuesForTime = function(currentTime)
 {
+  const time = math.fraction(currentTime);
   // If the last advance lead to a cycle end we evaluate possible
   // queueing and set data to the start of the next cycle
 
@@ -68,8 +84,16 @@ SequencePlayer.prototype.currentValues = function()
     }
     // This is a shortcut not taking into account there could be no data at
     // for 0/1 (for example "rotL 0.01 $ [1, 2]")
-    this.current_.values_ = this.sequence_ ? this.sequence_.dataAtIndex(0).values_ : undefined;
 
+    if (this.sequence_)
+    {
+      var firstSlice = this.sequence_.dataAtIndex(0);
+      this.current_.values_ = math.equal(firstSlice.time_, math.fraction(0)) ? firstSlice.values_ : null;
+    }
+    else
+    {
+      this.current_.values_ = null;
+    }
     this.queued_ = undefined;
     this.resetCycle_ = false;
   }
--- tests/test-sequence-player.js
+++ tests/test-sequence-player.js
@@ -8,13 +8,13 @@ var player = new SequencePlayer();
 var testAdvance = function(time, expectedTime, expectedValues)
 {
   assert.equal(fracToString(player.advance(time)), expectedTime);
-  if (expectedValues)
+  if (expectedValues !== undefined)
   {
-    assert.deepEqual(player.currentValues(), expectedValues)
+    assert.deepEqual(player.valuesForTime(time), expectedValues)
   }
 }
 
-// empty sequence reports next time to be on th enext cycle
+// empty sequence reports next time to be the next cycle's beginning
 
 testAdvance("2/3", "1/1");
 testAdvance("1/1", "2/1");
@@ -39,3 +39,9 @@ player.setSequence(sequence4)
 testAdvance("10/3", "11/3", ["c"]); // 3.(3) -> 3.(6); this is still playing the old cycle
 testAdvance("11/3", "4/1", ["d"]); // 3.(6) -> 4 ;  we hit cycle start and queue new one
 testAdvance("4/1", "9/2", ["e"]); // 4 -> 4.5; the slowed down cycle length_ should be taken into account
+
+// setting a sequence with no steps at 0 qhouldn't trigger it
+var sequence5 = evaluator.evaluate(quote+"~ scars filthier"+quote);
+player.setSequence(sequence5);
+testAdvance("11/2", "6/1", null); // Reporting the missing step's time is ok but no data should be sent
+testAdvance("6/1", "19/3", ["scars"]); // then move on to the next
