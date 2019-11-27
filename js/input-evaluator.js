15d58bd5f1e83a48bc9e5bfd9e73d97859a9253d
Mdashdotdashn <marc.nostromo@gmail.com>
2019-11-27 21:15:12 +0100

    compact rendering tree removing null properties

--- js/input-evaluator.js
+++ js/input-evaluator.js
@@ -1,5 +1,14 @@
 var peg = require("pegjs");
 var fs = require('fs');
+const _ = require('lodash');
+
+///////////////////////////////////////////////////////////////////////////////
+// from https://stackoverflow.com/questions/286141/remove-blank-attributes-from-an-object-in-javascript/
+
+const removeEmpty = e => e instanceof Object ? Object.entries(e).reduce((o, [k, v]) => {
+  if (typeof v === 'boolean' || v) o[k] = removeEmpty(v);
+  return o;
+}, e instanceof Array ? [] : {}) : e;
 
 ///////////////////////////////////////////////////////////////////////////////
 
@@ -12,5 +21,6 @@ Evaluator = function()
 Evaluator.prototype.evaluate = function(s)
 {
 	// Parses the command and returns a recursive tree of node stub
-  return this.parser.parse(s);
+  const raw = this.parser.parse(s);
+  return removeEmpty(raw);
 }
