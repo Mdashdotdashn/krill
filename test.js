339654cb3e2e541e9dd7b599a33a8fe7e89e7210
Marc nostromo <marc.nostromo@gmail.com>
2026-07-28 10:58:57 +0200

    Add console logs for the tests

--- test.js
+++ test.js
@@ -1,10 +1,16 @@
 var assert = require("assert");
 
+console.log("Running tests...");
 require("./tests/test-evaluator.js")
 require("./tests/test-ast-cases.js")
 require("./tests/test-parser-canonicalization.js")
+require("./tests/test-offline-evaluator-seam.js")
+console.log("Running test cases...");
 require("./tests/test-run-cases.js")
+console.log("Running test player state machine...");
 require("./tests/test-player-state-machine.js")
 require("./tests/test-render-query-contract.js")
+console.log("Running test render nodes...");
 require("./tests/test-render-nodes.js")
 require("./tests/test-harmony.js")
+console.log("done...");
